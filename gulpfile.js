4727dbd7477cbd76f138fae56f36a72b3247e7b0
ghj <w124543238@126.com>
2017-06-18 15:51:37 +0800

    add versions

--- gulpfile.js
+++ gulpfile.js
@@ -9,24 +9,15 @@ var cssnano=require('gulp-cssnano');   //压缩css
     imagemin=require('gulp-imagemin'), //压缩图片
     jshint=require('gulp-jshint'), //规范化js
     uglify=require('gulp-uglify'), //压缩js
-    browserSync=require('browser-sync')
-    clean=require('gulp-clean')  //清除目录
-    browserSync=require('browser-sync').create();
+    browserSync=require('browser-sync'),
+    clean=require('gulp-clean'),  //清除目录
+    browserSync=require('browser-sync').create(),　//启动服务，实现浏览器重载等
+    rev=require('gulp-rev'),   //添加版本号
+    revReplace=require('gulp-rev-replace')   //版本号替换
+    useref=require('gulp-useref')  //解析html资源定位
 
-gulp.task('reload',function(){
-    return browserSync.reload();
-})
 
-gulp.task('server',function(){                   //gulp server开启服务器
-    browserSync.init({
-        server: {
-            baseDir:"./"
-        }
-    });
-    gulp.watch(['**/.css','**/.js','**/*.html'],['reload'])   //监听文件，发生改变则自动重新加载
-})
-
-gulp.task('styles',function(){
+gulp.task('dist:css',function(){
     return gulp.src('./css/*.css')
         .pipe(concat('index-merge-3.css'))
         .pipe(autoprefixer('last 2 version'))
@@ -34,7 +25,7 @@ gulp.task('styles',function(){
         .pipe(gulp.dest('dist/css/'))
 })
 
-gulp.task('script',function(){
+gulp.task('dist:js',function(){
     return gulp.src('./js/*.js')
             .pipe(jshint())
             .pipe(jshint.reporter('default'))
@@ -55,7 +46,29 @@ gulp.task('clean',function(){
 })
 
 
+gulp.task('reload',function(){
+    return browserSync.reload();
+})
+
+gulp.task('server',function(){                   //gulp server开启服务器
+    browserSync.init({
+        server: {
+            baseDir:"./"
+        }
+    });
+    gulp.watch(['**/.css','**/.js','**/*.html'],['reload'])   //监听文件，发生改变则自动重新加载
+})
 
 gulp.task('default', ['clean'], function() {
-  gulp.start('styles','script','images');
+  gulp.start('dist:css','dist:css','images');
 });
+
+
+gulp.task("revision",['dist:css','dist:js'],function(){
+    return gulp.src(["dist/**/*.css","dist/**/*.js"])
+        .pipe(rev())   //添加版本号
+        .pipe(gulp.dest('dist'))
+        .pipe(rev.manifest()) //生成一个json文件记录版本号
+        .pipe(gulp.dest('dist'))
+})
+
